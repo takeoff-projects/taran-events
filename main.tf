21390ba1e7a032b0d6fb68e5eee1cdc2c4bd687b
OleksiiTaran <oleksii.taran@takeoff.com>
2021-09-30 11:56:07 +0300

    terraform updates

--- main.tf
+++ main.tf
@@ -1,54 +1,42 @@
-# main.tf
+provider "google" {
+  project = var.project_id
+  region  = var.provider_region
+}
 
-terraform {
-    required_version = ">=0.14"
-
-    required_providers {
-    # Cloud Run support was added on 3.3.0
-    google = ">= 3.3"
-  }
-    provider "google" {
-        project = "roi-takeoff-user10"
-  }
-
-    resources "google_ project_service" "run_api" {
-        service = "run.googleapis.com"
-
-        disable_on_destroy = true
-    }
-
-    # Create the Cloud Run service
-resource "google_cloud_run_service" "run_service" {
-  name = "app"
+resource "google_cloud_run_service" "events-website" {
+  name     = "events-website"
   location = "us-central1"
 
   template {
     spec {
       containers {
         image = "gcr.io/roi-takeoff-user10/events-app:latest"
+        env {
+          name = "GOOGLE_CLOUD_PROJECT"
+          value = var.project_id
       }
+        env {
+          name = "BACKEND_URL"
+          value = "https://events-api-4kad4w6jba-uc.a.run.app/events"
+        }
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
   }
-
-  # Waits for the Cloud Run API to be enabled
-  depends_on = [google_project_service.run_api]
 }
 
-# Allow unauthenticated users to invoke the service
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.run_service.name
-  location = google_cloud_run_service.run_service.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
 }
 
-output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
-}
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.events-website.location
+  project     = google_cloud_run_service.events-website.project
+  service     = google_cloud_run_service.events-website.name
 
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
